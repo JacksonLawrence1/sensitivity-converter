adfd0dd54e6c2655e4096fcf553667ba5df69bce
Jackson Lawrence <fi19710@bristol.ac.uk>
2023-07-13 19:08:08 +0100

    Updates

---
+++ server/app/page.js
@@ -0,0 +1,493 @@
+(() => {
+var exports = {};
+exports.id = 931;
+exports.ids = [931];
+exports.modules = {
+
+/***/ 8038:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/compiled/react");
+
+/***/ }),
+
+/***/ 8704:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/compiled/react-dom/server-rendering-stub");
+
+/***/ }),
+
+/***/ 7897:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/compiled/react-server-dom-webpack/client");
+
+/***/ }),
+
+/***/ 6786:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/compiled/react/jsx-runtime");
+
+/***/ }),
+
+/***/ 1844:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/server/app-render/get-segment-param");
+
+/***/ }),
+
+/***/ 6624:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/server/future/helpers/interception-routes");
+
+/***/ }),
+
+/***/ 7085:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/app-router-context");
+
+/***/ }),
+
+/***/ 199:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/hash");
+
+/***/ }),
+
+/***/ 9569:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/hooks-client-context");
+
+/***/ }),
+
+/***/ 893:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/add-path-prefix");
+
+/***/ }),
+
+/***/ 7887:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/handle-smooth-scroll");
+
+/***/ }),
+
+/***/ 8735:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/is-bot");
+
+/***/ }),
+
+/***/ 8231:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/parse-path");
+
+/***/ }),
+
+/***/ 3750:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/remove-trailing-slash");
+
+/***/ }),
+
+/***/ 9618:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/server-inserted-html");
+
+/***/ }),
+
+/***/ 6742:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   GlobalError: () => (/* reexport default from dynamic */ next_dist_client_components_error_boundary__WEBPACK_IMPORTED_MODULE_0___default.a),
+/* harmony export */   __next_app__: () => (/* binding */ __next_app__),
+/* harmony export */   originalPathname: () => (/* binding */ originalPathname),
+/* harmony export */   pages: () => (/* binding */ pages),
+/* harmony export */   tree: () => (/* binding */ tree)
+/* harmony export */ });
+/* harmony import */ var next_dist_client_components_error_boundary__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1865);
+/* harmony import */ var next_dist_client_components_error_boundary__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(next_dist_client_components_error_boundary__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_dist_server_app_render_entry_base__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(6327);
+/* harmony import */ var next_dist_server_app_render_entry_base__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_dist_server_app_render_entry_base__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony reexport (unknown) */ var __WEBPACK_REEXPORT_OBJECT__ = {};
+/* harmony reexport (unknown) */ for(const __WEBPACK_IMPORT_KEY__ in next_dist_server_app_render_entry_base__WEBPACK_IMPORTED_MODULE_1__) if(["default","tree","pages","GlobalError","originalPathname","__next_app__"].indexOf(__WEBPACK_IMPORT_KEY__) < 0) __WEBPACK_REEXPORT_OBJECT__[__WEBPACK_IMPORT_KEY__] = () => next_dist_server_app_render_entry_base__WEBPACK_IMPORTED_MODULE_1__[__WEBPACK_IMPORT_KEY__]
+/* harmony reexport (unknown) */ __webpack_require__.d(__webpack_exports__, __WEBPACK_REEXPORT_OBJECT__);
+
+    const tree = {
+        children: [
+        '',
+        {
+        children: ['__PAGE__', {}, {
+          page: [() => Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 4074)), "E:\\zekro\\Documents\\Codes\\sensitivity-converter\\src\\app\\page.js"],
+          
+        }]
+      },
+        {
+          'layout': [() => Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 5605)), "E:\\zekro\\Documents\\Codes\\sensitivity-converter\\src\\app\\layout.js"],
+          
+        }
+      ]
+      }.children;
+    const pages = ["E:\\zekro\\Documents\\Codes\\sensitivity-converter\\src\\app\\page.js"];
+    
+    const originalPathname = "/page"
+    const __next_app__ = {
+      require: __webpack_require__,
+      // all modules are in the entry chunk, so we never actually need to load chunks in webpack
+      loadChunk: () => Promise.resolve()
+    }
+
+    
+  
+
+/***/ }),
+
+/***/ 5376:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 8709, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 2698, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 7833, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 9150, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 7734, 23))
+
+/***/ }),
+
+/***/ 6876:
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ 3333:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 1389))
+
+/***/ }),
+
+/***/ 1389:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ body)
+});
+
+// EXTERNAL MODULE: external "next/dist/compiled/react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(6786);
+// EXTERNAL MODULE: external "next/dist/compiled/react"
+var react_ = __webpack_require__(8038);
+;// CONCATENATED MODULE: ./src/app/games.js
+const games = [
+    {
+        id: "csgo",
+        name: "Counter-Strike: Global Offensive",
+        convert: {
+            csgo: (sens)=>sens,
+            valorant: (sens)=>sens / 3.18,
+            osu: (sens)=>sens / 2.50947,
+            r6: (sens)=>sens * 3.83972336439,
+            overwatch: (sens)=>sens / 3.33333333333,
+            b4b: (sens)=>sens / 9.43,
+            aimlab: (sens)=>sens * 2.273
+        }
+    },
+    {
+        id: "valorant",
+        name: "Valorant",
+        convert: {
+            csgo: (sens)=>sens * 3.18
+        }
+    },
+    {
+        id: "osu",
+        name: "osu!",
+        convert: {
+            csgo: (sens)=>sens * 2.50947
+        }
+    },
+    {
+        id: "r6",
+        name: "Tom Clancy's Rainbow Six Siege",
+        convert: {
+            csgo: (sens)=>sens / 3.83972336439
+        }
+    },
+    {
+        id: "overwatch",
+        name: "Overwatch",
+        convert: {
+            csgo: (sens)=>sens * 3.33333333333
+        }
+    },
+    {
+        id: "b4b",
+        name: "Back 4 Blood",
+        convert: {
+            csgo: (sens)=>sens * 9.43
+        }
+    },
+    {
+        id: "aimlab",
+        name: "Aim Lab",
+        convert: {
+            csgo: (sens)=>sens / 2.273
+        }
+    }
+];
+
+;// CONCATENATED MODULE: ./src/app/body.js
+/* __next_internal_client_entry_do_not_use__ default auto */ 
+
+
+function Body(props) {
+    const [sens, setSens] = (0,react_.useState)("1");
+    const [targetSens, setTargetSens] = (0,react_.useState)(undefined);
+    const [game, setGame] = (0,react_.useState)(games[0]);
+    const [targetGame, setTargetGame] = (0,react_.useState)(games[0]);
+    function mapGames() {
+        // map games to dropdown options
+        return games.map((game)=>{
+            return /*#__PURE__*/ jsx_runtime_.jsx("option", {
+                children: game.name
+            }, game.id);
+        });
+    }
+    function gameDropdown(prefix, onChange) {
+        return /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+            children: [
+                /*#__PURE__*/ jsx_runtime_.jsx("p", {
+                    children: prefix
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("select", {
+                    className: "dropdown",
+                    onChange: onChange,
+                    children: mapGames()
+                })
+            ]
+        });
+    }
+    function handleChange(e) {
+        // make sure the input is a number
+        if (isNaN(e.target.value)) {
+            return;
+        }
+        setSens(e.target.value);
+    }
+    function displayResult() {
+        if (sens === null) {
+            return;
+        }
+        return /*#__PURE__*/ jsx_runtime_.jsx("p", {
+            children: targetSens
+        });
+    }
+    const findGame = (name)=>{
+        return games.find((g)=>g.name === name);
+    };
+    (0,react_.useEffect)(()=>{
+        if (sens === undefined) {
+            return;
+        }
+        if (game === targetGame) {
+            setTargetSens(sens);
+            return;
+        }
+        // convert sens to csgo sens as a baseline
+        let targetSensitivity = game.convert.csgo(sens);
+        // convert csgo sens to target sens if target isn't csgo
+        if (targetGame.id !== "csgo") {
+            targetSensitivity = games[0].convert[targetGame.id](targetSensitivity);
+        }
+        setTargetSens(targetSensitivity);
+    }, [
+        sens,
+        game,
+        targetGame
+    ]);
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+        children: [
+            /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                className: "generic-card",
+                children: [
+                    /*#__PURE__*/ jsx_runtime_.jsx("p", {
+                        className: "text-2xl",
+                        children: "This website is designed to help convert your sensitivity between games."
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx("p", {
+                        className: "text-2xl",
+                        children: " Select a game from the dropdown with your preferred sensitivity, and desired game you want the equivalent sensitivty for."
+                    })
+                ]
+            }),
+            /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                className: "generic-card",
+                children: [
+                    gameDropdown("Choose your source game: ", (e)=>setGame(findGame(e.target.value))),
+                    gameDropdown("Choose your target game: ", (e)=>setTargetGame(findGame(e.target.value))),
+                    /*#__PURE__*/ jsx_runtime_.jsx("p", {
+                        children: "Game Sensitivity: "
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx("input", {
+                        className: "generic-input",
+                        value: sens,
+                        onChange: handleChange
+                    }),
+                    displayResult()
+                ]
+            })
+        ]
+    });
+}
+/* harmony default export */ const body = (Body);
+
+
+/***/ }),
+
+/***/ 5605:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ RootLayout),
+/* harmony export */   metadata: () => (/* binding */ metadata)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(6786);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_font_google_target_css_path_src_app_layout_js_import_Inter_arguments_subsets_latin_variableName_inter___WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(3773);
+/* harmony import */ var next_font_google_target_css_path_src_app_layout_js_import_Inter_arguments_subsets_latin_variableName_inter___WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_font_google_target_css_path_src_app_layout_js_import_Inter_arguments_subsets_latin_variableName_inter___WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _globals_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(5553);
+/* harmony import */ var _globals_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_globals_css__WEBPACK_IMPORTED_MODULE_1__);
+
+
+
+const metadata = {
+    title: "Create Next App",
+    description: "Generated by create next app"
+};
+function RootLayout({ children }) {
+    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("html", {
+        lang: "en",
+        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("body", {
+            className: (next_font_google_target_css_path_src_app_layout_js_import_Inter_arguments_subsets_latin_variableName_inter___WEBPACK_IMPORTED_MODULE_2___default().className),
+            children: children
+        })
+    });
+}
+
+
+/***/ }),
+
+/***/ 4074:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ Home)
+});
+
+// EXTERNAL MODULE: external "next/dist/compiled/react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(6786);
+;// CONCATENATED MODULE: ./src/app/header.js
+
+function Header() {
+    return /*#__PURE__*/ jsx_runtime_.jsx("header", {
+        children: /*#__PURE__*/ jsx_runtime_.jsx("h1", {
+            className: "mt-6 text-center font-bold text-red-500 text-5xl mb-16",
+            children: "Game Sensitivity Converter"
+        })
+    });
+}
+/* harmony default export */ const header = (Header);
+
+// EXTERNAL MODULE: ./node_modules/next/dist/build/webpack/loaders/next-flight-loader/module-proxy.js
+var module_proxy = __webpack_require__(1913);
+;// CONCATENATED MODULE: ./src/app/body.js
+
+const proxy = (0,module_proxy.createProxy)(String.raw`E:\zekro\Documents\Codes\sensitivity-converter\src\app\body.js`)
+
+// Accessing the __esModule property and exporting $$typeof are required here.
+// The __esModule getter forces the proxy target to create the default export
+// and the $$typeof value is for rendering logic to determine if the module
+// is a client boundary.
+const { __esModule, $$typeof } = proxy;
+const __default__ = proxy.default;
+
+
+/* harmony default export */ const body = (__default__);
+;// CONCATENATED MODULE: ./src/app/page.js
+
+
+
+function Home() {
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)("main", {
+        children: [
+            /*#__PURE__*/ jsx_runtime_.jsx(header, {}),
+            /*#__PURE__*/ jsx_runtime_.jsx(body, {})
+        ]
+    });
+}
+
+
+/***/ }),
+
+/***/ 5553:
+/***/ (() => {
+
+
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [608], () => (__webpack_exec__(6742)));
+module.exports = __webpack_exports__;
+
+})();
